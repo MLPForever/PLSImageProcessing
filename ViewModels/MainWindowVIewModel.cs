4d625a5156a27247da93e7e248169a7a3ea63d83
MLPForever <75858634+MLPForever@users.noreply.github.com>
2025-05-17 17:18:01 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowVIewModel.cs
@@ -0,0 +1,32 @@
+﻿using BaseTemplateForWPF;
+using OpenCvSharp;
+
+namespace PLSImageProcessing.ViewModels
+{
+    public class MainWindowVIewModel : BaseViewModel
+    {
+
+		#region Title 
+
+		private string _Title = "PLSImageProcessing";
+		public string Title
+        {
+			get => _Title;
+			set => Set(ref _Title, value);
+		}
+
+		#endregion
+
+
+		#region Photo 
+
+		private Mat _Photo;
+		public Mat Photo
+        {
+			get => _Photo;
+			set => Set(ref _Photo, value);
+		}
+
+		#endregion
+	}
+}
